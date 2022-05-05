29b1955d717541d6a481d51333d4377fad401114
Ryan White <ryanwhite04@gmail.com>
2022-05-06 02:56:31 +0800

    Remove lit-element dependancy

--- static/selection-puzzle.js
+++ static/selection-puzzle.js
@@ -1,5 +1,3 @@
-import {html, css, LitElement} from "https://unpkg.com/lit-element/lit-element.js?module"
-
 // check('abcd')('x', 1) == 0 // not found
 // check('abcd')('a', 1) == 1 // found but not correct
 // check('abcd')('b', 1) == 2 // correct
@@ -7,22 +5,66 @@ function check(string) {
     return (v, i) => string.includes(v) ? string[i] == v ? 2 : 1 : 0;
 }
 
-export default class SelectionPuzzle extends LitElement {
-
-    static properties = {
-        answer: String,
-        disableIncorrect: {
-            type: Boolean,
-            attribute: 'disable-incorrect',
-        },
-        attempts: Array,
-        attempt: Object,
+const styles = document.createElement('style');
+styles.textContent = `
+    :host {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-column-gap: 0.5em;
+        grid-row-gap: 0.5em;
+        margin: 1em;
+    }
+    .choice {
+        height: 3em;
+        width: 3em;
+        color: black;
+    }
+    .choice[found] {
+        background: yellow;
+    }
+    .choice[correct] {
+        background: green;
+    }
+    ::slotted([slot="option"]) {
+        height: 3em;
+        width: 3em;
+        text-align: center;
+    }
+`;
+
+export default class SelectionPuzzle extends HTMLElement {
+
+    static get observedAttributes() {
+        return ["answer", "disable-incorrect"]
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log('attributeChanged', { name, oldValue, newValue })
+        if (name == "disable-incorrect") {
+            this.disableIncorrect = newValue !== null;
+        } else {
+            this[name] = newValue;
+        }
+    }
+
+    state = "playing";
+    attempt = {
+        options: [],
+        choices: [],
+    };
+    disableIncorrect = false;
 
     constructor() {
         super();
-        this.attempt = { options: [] };
         this.attempts = [this.attempt];
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(styles);
+        this.render(this.shadowRoot);
     }
 
     submit(attempt) {
@@ -30,21 +72,21 @@ export default class SelectionPuzzle extends LitElement {
             .map(option => option.textContent) // get the value of each option
             .map(check(this.answer)) // check if the value is found or correct in the answer
 
-        // Hide disable options that were selected but not found
-        this.disableIncorrect &&  // only if the disableIncorrect option is set
-            attempt.options.forEach((option, i) => option.disabled = !attempt.value[i])
-
-        this.attempt = { options: [] };
-        this.attempts.push(this.attempt);
-
-        return attempt.value.every(v => v == 2) // word is correct
-            || this.attempts.length > 4 // or no attempts left
+        attempt.choices.forEach((choice, i) => {
+            choice.disabled = true;
+            attempt.value[i] == 1 && choice.setAttribute("found", "");
+            attempt.value[i] == 2 && choice.setAttribute("correct", "");
+        });
+        attempt.options.forEach((option, i) => {
+            option.disabled = this.disableIncorrect && !attempt.value[i]
+        })
+        return attempt.value.every(v => v == 2)
     }
 
     // End the game
     finish(success) {
-        this.shadowRoot
-            .getElementById('options') // the slot that the options are in
+        this[success ? "success" : "failure"].removeAttribute('hidden');
+        this.options // the slot that the options are in
             .assignedElements() // the options in the light dom
             .forEach(option => option.disabled = true); // disable them all to end game
         this.dispatchEvent(new CustomEvent('complete', {
@@ -52,72 +94,67 @@ export default class SelectionPuzzle extends LitElement {
         }))
     }
 
-    choose(event) {
+    select(event) {
         const { target: option } = event
         if (option.slot === 'option') {
             option.disabled = true;
             this.attempt.options.push(option);
-                this.attempt.options.length == 4 &&
-                this.submit(this.attempt) &&
-                this.finish();
-            this.requestUpdate();
+            this.choose(option, this.attempt);
+            if (this.attempt.options.length == 4) {
+                if (this.submit(this.attempt)) this.finish(true);
+                else if (this.attempts.length == 4) this.finish(false);
+                else this.attempts.push(this.attempt = {
+                    options: [],
+                    choices: [],
+                });
+            }
         }
     }
-
-    remove(option) {
-        option.disabled = false;
-        this.attempt.options.splice(this.attempt.options.indexOf(option), 1);
-        this.requestUpdate();
-    }
-
-    static styles = css`
-        :host {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        #options, #choices {
-            display: grid;
-            grid-template-columns: repeat(4, 1fr);
-            grid-column-gap: 0.5em;
-            grid-row-gap: 0.5em;
-            margin: 1em;
-        }
-        .choice {
-            height: 3em;
-            width: 3em;
-            color: black;
-        }
-        .choice[found] {
-            background: yellow;
-        }
-        .choice[correct] {
-            background: green;
-        }
-        ::slotted([slot="option"]) {
-            height: 3em;
-            width: 3em;
-            text-align: center;
-        }
-    `;
 
     render() {
-        const choices = this.attempts
-            .map(({ options, value }) => options.map(this.choice(value)));
-        return html`
-            <slot></slot>
-            <slot id="options" @click=${this.choose} name="option"></slot>
-            <div id="choices">${choices}</div>`
+        const slot = document.createElement('slot');
+
+        this.options = document.createElement('slot');
+        this.options.setAttribute('name', 'option');
+        this.options.addEventListener('click', this.select.bind(this));
+        this.options.classList.add("grid");
+
+        this.choices = document.createElement('div');
+        this.choices.classList.add("grid");
+
+        this.success = document.createElement('slot');
+        this.success.setAttribute("name", "success");
+        this.success.textContent = "Success";
+        this.success.setAttribute("hidden", "");
+
+        this.failure = document.createElement('slot');
+        this.failure.setAttribute("name", "failure");
+        this.failure.textContent = "Failure";
+        this.failure.setAttribute("hidden", "");
+
+        this.shadowRoot.append(
+            slot,
+            this.options,
+            this.choices,
+            this.success,
+            this.failure
+        );
     }
 
-    choice(value) {
-        return (option, i) => value ? // if attempt has been submitted it will have a value
-            html`<button class="choice" part="choice"
-                disabled ?found=${value[i] == 1} ?correct=${value[i] == 2}
-                >${option.textContent}</button>` :
-            html`<button class="choice" part="choice"
-                @click=${() => this.remove(option)}
-                >${option.textContent}</button>`
+    choose(option, attempt) {
+        const choice = document.createElement("button");
+        choice.classList.add("choice");
+        choice.setAttribute("part", "choice");
+        choice.textContent = option.textContent;
+        const remove = () => {
+            option.disabled = false;
+            attempt.options.splice(attempt.options.indexOf(option), 1);
+            this.choices.removeChild(choice);
+            attempt.choices.splice(attempt.choices.indexOf(choice), 1);
+        }
+        choice.addEventListener("click", remove)
+        attempt.choices.push(choice);
+        this.choices.append(choice);
     }
 }
 
