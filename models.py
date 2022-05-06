70377434d72314116d40ff0dcba70dea28efe29f
Ryan White <ryanwhite04@gmail.com>
2022-05-06 17:30:46 +0800

    Create games table
    This is not strictly necessary, but I'll put it in for now to stop any
    issue that arise form adding/removing puzzles (which would change the
    puzzle chosen on past days since it's based on the number of puzzles
    modulus a number) This can probably be removed once we have a good way
    of adding/removing puzzles

--- models.py
+++ models.py
@@ -14,14 +14,22 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-class Play(db.Model):
-    __tablename__ = 'games'
+class Game(db.Model):
+    __tablename__ = "games"
     id = db.Column(db.Integer, primary_key=True)
-
-    # User Authentication fields
-    user = db.Column(db.Integer, nullable=False)
-    time = db.Column(db.DateTime(), nullable=False)
     word = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
+        return f'<Game {self.word}>'
+
+class Play(db.Model):
+    __tablename__ = 'plays'
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, nullable=False)
+    game = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.DateTime(), nullable=False)
+    word = db.Column(db.String(255), nullable=False)
+    
+    def __repr__(self):
         return f'<Play {self.word} at {self.time}>'
+
