ebe7b7346a7d0c002e7cc56dbd70763757be9706
Ryan White <ryanwhite04@gmail.com>
2022-05-06 17:52:41 +0800

    Define relationships between User, Game, and Play
    There is a many to one relationship between play and user
    There is a many to one relationship between play and game

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from app import db
 
 # From https://flask-user.readthedocs.io/en/latest/data_models.html
 class User(db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
 
     # User Authentication fields
@@ -10,26 +9,26 @@ class User(db.Model):
     email_confirmed_at = db.Column(db.DateTime())
     username = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    plays = db.relationship("Play", backref="user", lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
 
 class Game(db.Model):
-    __tablename__ = "games"
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(255), nullable=False)
+    plays = db.relationship("Play", backref="game", lazy=True)
 
     def __repr__(self):
         return f'<Game {self.word}>'
 
 class Play(db.Model):
-    __tablename__ = 'plays'
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.Integer, nullable=False)
-    game = db.Column(db.Integer, nullable=False)
-    time = db.Column(db.DateTime(), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"))
+    game = db.Column(db.Integer, db.ForeignKey("game.id"))
+    time = db.Column(db.DateTime(), index=True)
     word = db.Column(db.String(255), nullable=False)
-    
+
     def __repr__(self):
         return f'<Play {self.word} at {self.time}>'
 
