da07b45a1c9d50a743b0fe4b1b8d9a37bcecad37
Justin Duong <egjustin123456@gmail.com>
2022-05-11 19:03:31 +0800

    Add validations to username email and password

--- Registration.py
+++ Registration.py
@@ -1,9 +1,10 @@
 from ast import Pass
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, EqualTo, Length
 class Registration(FlaskForm):
-    username = StringField(label= 'Username')
-    email = StringField(label='Email Address')
-    password = PasswordField(label="Password")
-    Confirm_password= PasswordField(label='Confirm Password') 
-    submitbutton = SubmitField(label='Create Account')
+    username = StringField(label= 'Username', validators = [Length(min = 3, max = 40)])
+    email = StringField(label='Email Address', validators=[Email()])
+    password = PasswordField(label="Password", validators=[Length(min = 10)])
+    Confirm_password= PasswordField(label='Confirm Password', validators=[EqualTo("password")]) 
+    submitbutton = SubmitField(label='Register Now!')
