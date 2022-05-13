ff3da5da41a836a92eac6311906c9cf2ea584d2c
Justin Duong <egjustin123456@gmail.com>
2022-05-13 19:04:47 +0800

    add a new form to handle log in

--- registration.py
+++ registration.py
@@ -1,11 +1,17 @@
 from ast import Pass
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email, EqualTo, Length
+from wtforms.validators import Email, EqualTo, Length, DataRequired
 
 class Registration(FlaskForm):
-    username = StringField(label='Username', validators=[Length(min = 1, max = 40)])
-    email = StringField(label='Email Address', validators=[Email()])
-    password = PasswordField(label="Password", validators=[Length(min = 1)])
-    confirm = PasswordField(label='Confirm Password', validators=[EqualTo("password")])
+    username = StringField(label='Username', validators=[Length(min = 1, max = 40),  DataRequired()])
+    email = StringField(label='Email Address', validators=[Email(),  DataRequired()])
+    password = PasswordField(label="Password", validators=[Length(min = 1),  DataRequired()])
+    confirm = PasswordField(label='Confirm Password', validators=[EqualTo("password"),  DataRequired()])
     submit = SubmitField(label='Register')
+
+    
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
