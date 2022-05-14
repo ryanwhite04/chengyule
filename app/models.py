4c7b00c255574f62513278b5067af805c85eece8
Ryan White <ryanwhite04@gmail.com>
2022-05-15 00:27:11 +0800

    Store plays with user.play
    Using play.word as a primary key caused duplicate games to be returned
    for user.games when user played same game multiple times. To correct
    this, there is now only 1 play/game, which gets updated on subsequent
    plays by changing the word on the play, aswell as incrementing
    "attempts" and setting whether or not the word was correct
    
    This involved the use of session begin/flush/add/commit/rollback in the
    user.play function to ensure bad plays aren't commited to the database
    
    The ensure user.play works correct, make sure to commit and additions to
    the session before calling user.play, or session.begin inside it will
    return an error

--- app/models.py
+++ app/models.py
@@ -16,11 +16,33 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     games = association_proxy("plays", "game")
+
+
     def play(self, game, word):
-        p = Play(word=word)
-        self.plays.append(p)
-        game.plays.append(p)
-        return p
+        """
+        If user has already played this game
+            return existing play
+        else create new play
+        set play word
+        """
+        db.session.begin()
+        # this ensures game/user have ids for the following query
+        db.session.add(game)
+        db.session.add(self)
+        db.session.flush()
+        p = Play.query.get({
+            "user_id": self.id,
+            "game_id": game.id,
+        }) or Play(game=game, user=self)
+        try:
+            # will fail if word doesn't obey rules
+            # This is checked in the validator for word in Play
+            p.word = word
+            db.session.commit()
+            return p
+        except:
+            db.session.rollback()
+            raise
 
     def checkPassword(self, password):
         return check_password_hash(self.password, password)
@@ -37,11 +59,12 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(255), nullable=False)
     users = association_proxy("plays", "user")
-    @validates("plays")
-    def check(self, key, play):
-        if len(self.word) != len(play.word):
-            raise ValueError("Can't play that word", self.word, play.word)
-        return play
+    attempts = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, **kwargs):
+        super(Game, self).__init__(**kwargs)
+        self.attempts = 4
+    
     def __repr__(self):
         return f'<Game {self.word}>'
 
@@ -49,9 +72,29 @@ class Play(db.Model):
     __tablename__ = "plays"
     user_id = db.Column(db.ForeignKey("users.id"), primary_key=True)
     game_id = db.Column(db.ForeignKey("games.id"), primary_key=True)
-    word = db.Column(db.String(255), nullable=False, primary_key=True)
+    word = db.Column(db.String(255), nullable=False)
     user = db.relationship("User", backref="plays")
     game = db.relationship("Game", backref="plays")
+    attempt = db.Column(db.Integer, nullable=False)
+    correct = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, **kwargs):
+        super(Play, self).__init__(**kwargs)
+        self.attempt = 0
+        self.correct = False
+
+    @validates("word")
+    def check(self, key, word):
+        if len(self.game.word) != len(word):
+            raise ValueError("Can't play that word", word, self.game.word)
+        if self.game.attempts == self.attempt:
+            raise Exception("Maximum attempts reached", self.game.attempts, self.attempt)
+        if self.correct:
+            raise Exception("This game is already complete")
+        if self.game.word == word:
+            self.correct = True
+        self.attempt += 1
+        return word
 
     def __repr__(self):
         return f'<Play word={self.word} user={self.user.username} game={self.game.word}>'
--- test.py
+++ test.py
@@ -27,16 +27,14 @@ class UserModelCase(TestCase):
         self.assertTrue(u.checkPassword("a"))
         self.assertFalse(u.checkPassword("b"))
     
-    def test_multiplePlays(self):
+    def test_secondPlay(self):
         u = User(username="a", email="a@a.a", password="a")
         g = Game(word="a")
-        db.session.add(u)
-        db.session.add(g)
-        db.session.add(u.play(g, "a"))
-        db.session.add(u.play(g, "b"))
-        db.session.commit()
-        plays = Play.query.all()
-        print(f"{plays=}")
+        p = u.play(g, "b")
+        u.play(g, "c") # this changes the first p
+        self.assertCountEqual(Play.query.all(), (p,))
+        self.assertCountEqual(User.query.all(), (u,))
+        self.assertCountEqual(Game.query.all(), (g,))
 
     def test_wrongWordLength(self):
         u = User(username="a", email="a@a.a", password="a")
@@ -44,7 +42,18 @@ class UserModelCase(TestCase):
         with self.assertRaises(ValueError) as context:
             u.play(g, "aa")
         self.assertTrue("Can't play that word" in str(context.exception))
-
+        db.session.add(u)
+        db.session.commit()
+        self.assertFalse(Play.query.all())
+        self.assertCountEqual(User.query.all(), (u,))
+        self.assertFalse(Game.query.all())
+    
+    def show(self):
+        plays = Play.query.all()
+        users = User.query.all()
+        games = Game.query.all()
+        print(f"{plays=}, {users=}, {games=}")
+    
     def populate(self):
         users = (
             User(username="a", email="a@a.a", password="a"),
@@ -52,18 +61,16 @@ class UserModelCase(TestCase):
             User(username="c", email="c@c.c", password="c"),
         )
         games = (
-            Game(word="a"),
-            Game(word="b"),
-            Game(word="c"),
+            Game(word="x"),
+            Game(word="y"),
+            Game(word="z"),
         )
 
-        db.session.add_all(games)
-        db.session.add_all(users)
         for i, user in enumerate(users):
-            db.session.add(user.play(games[i], ["a", "b", "c"][i]))
-            db.session.add(user.play(games[(i+1)%len(games)], ["a", "b", "c"][i]))
+            user.play(games[i], ["a", "b", "c"][i])
+            user.play(games[i], ["d", "e", "f"][i])
+            user.play(games[(i+1)%len(games)], ["a", "b", "c"][i])
 
-        db.session.commit()
         return games, users
 
     def test_playGame(self):
