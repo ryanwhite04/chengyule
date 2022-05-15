a7e585505e845dd4bd905852368af3feb56e5529
Ryan White <ryanwhite04@gmail.com>
2022-05-16 03:22:13 +0800

    Fix database protocol
    Heroku uses "postgres" as the database protocol, but this is deprecated
    in the engine that flask is using to connect to the postgresql database
    and now must be "postgresql", so this just swaps them out

--- config.py
+++ config.py
@@ -3,10 +3,15 @@ from os.path import abspath, dirname, join
 from os import environ
 
 load_dotenv(join(abspath(dirname(__file__)), ".env"))
+def database_URL():
+    url = environ.get("DATABASE_URL") or "sqlite:///../app.db"
+    if url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://")
+    return url
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL") or "sqlite:///../app.db"
+    SQLALCHEMY_DATABASE_URI = database_URL()
     SECRET_KEY = environ.get("SECRET_KEY") or "SECRET_KEY"
     TRANSLATION_KEY = environ.get("TRANSLATION_KEY") or "TRANSLATION_KEY"
 
