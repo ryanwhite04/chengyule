2aae65fd17f8333b54f2e41f90feeb67f4d60a2a
Ryan White <ryanwhite04@gmail.com>
2022-05-16 04:30:27 +0800

    Remove main function from chengyu.py

--- app/chengyu.py
+++ app/chengyu.py
@@ -7,7 +7,4 @@ def getIndex(digest, index, length, mod):
 def select(path, key, count=1):
     options = loads(open(path, encoding ="utf-8").read())
     digest = sha256(bytes(key)).digest()
-    return [options[getIndex(digest, i*2, 2, len(options))] for i in range(count)]  
-
-if __name__ == "__main__":
-    print(select('static/chengyu.json'))
+    return [options[getIndex(digest, i*2, 2, len(options))] for i in range(count)]
