bbf67771fb84c006e01775ecc9b5c98261766a75
Ryan White <ryanwhite04@gmail.com>
2022-05-23 03:17:04 +0800

    Use chengyu dictionary instead of list

--- app/chengyu.py
+++ app/chengyu.py
@@ -5,6 +5,6 @@ def getIndex(digest, index, length, mod):
     return int.from_bytes(digest[index:index+length], 'little') % mod
 
 def select(path, key, count=1):
-    options = loads(open(path, encoding ="utf-8").read())
+    options = list(loads(open(path, encoding ="utf-8").read()).keys())
     digest = sha256(bytes(key)).digest()
     return [options[getIndex(digest, i*2, 2, len(options))] for i in range(count)]
