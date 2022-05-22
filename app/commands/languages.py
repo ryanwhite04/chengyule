fdc1992bf615439115d7ee5404b2da3520f26b3d
Ryan White <ryanwhite04@gmail.com>
2022-05-23 06:08:48 +0800

    Extend language cli
    Upgrade now syncs the db with local database
    Allowed languages are stored in "languages/allowed" default on top line
    
    Install will sync database with this list of which are allowed/default
    
    Upgade will first ensure all languages are on databasae, then install
    
    Languages can be easily added/removed/set to default by editing the file
    "languages/allowed"
    
    or can be done with add/remove/default commands by passing language code
    
    Any changes to allowed list through cli are automatically followed by
    install to ensure changes are reflected in database

--- app/commands/languages.py
+++ app/commands/languages.py
@@ -10,26 +10,12 @@ from click import argument, File, pass_context
 
 class Cli:
 
-    def init_app(self, app, db):
-        from app.models import Code, Text
-        @app.cli.group()
-        @pass_context
-        def languages(context):
-            """Translation and localization commands"""
-            pass
+    def __init__(self):
 
-        @languages.command()
-        @argument("output", default="languages/index.json")
-        def update(output):
-            """
-            Update list of available languages
-            Saved to OUTPUT
-            Example:
-            >>>flask languages update languages.json
-            """
+        def update(output, key):
             json = post("https://translation.googleapis.com/language/translate/v2/languages", {
                 "target": "zh",
-                "key": app.config["TRANSLATION_KEY"],
+                "key": key,
             }).json()
             try:
                 languages = json["data"]["languages"]
@@ -49,17 +35,34 @@ class Cli:
                 data = json["error"]["message"]
                 stderr.write(data)
             except: raise
-
-
-
-        @languages.command()
-        def list():
-            """
-            List installed languages
-            These locales are in the database and will offer a menu option to clients
-            """
+        
+        def list(Code):
             try:
-                [print(code) for code in Code.query.all()]
+                return [code for code in Code.query.all() if code.allowed]
+            except (ProgrammingError, OperationalError) as e:
+                print("""
+                    Code table not in database,
+                    Try updating database first with 
+                    >>>flask db upgrade
+                """)
+            except: raise
+        
+        def upgrade(db, Text, Code):
+            available = loads(open("languages/index.json").read())
+            settings = open("languages/allowed")
+            default = settings.readline()
+            allowed = settings.readlines()
+            allowed.append(default)
+            try:
+                for code, name in available.items():
+                    try:
+                        db.session.add(Text(name))
+                        db.session.flush()
+                        db.session.add(Code(code, name))
+                        db.session.commit()
+                    except IntegrityError as e:
+                        db.session.rollback()
+                        # print(f"{name}, {name} is already installed", file=stderr)
             except (ProgrammingError, OperationalError) as e:
                 print("""
                     Code table not in database,
@@ -68,26 +71,164 @@ class Cli:
                 """)
             except: raise
 
+        def install(db, Code):
+            file = open("languages/allowed")
+            default = file.readline()
+            allowed = file.readlines()
+            allowed.append(default)
+
+            # Reset
+            for code in Code.query.where(Code.allowed).all():
+                code.allowed = False
+                code.default = False
+            
+            # Set
+            for code in allowed:
+                try:
+                    c = Code.query.get(code.strip())
+                    c.allowed = True
+                    if code == default: c.default = True
+                    db.session.add(c)
+                    db.session.commit()
+                except IntegrityError as e:
+                    db.session.rollback()
+                    print(f"{code} is not available", file=stderr)
+                except: raise
+
+        def add(path, code):
+            codes = [line.strip() for line in open(path).readlines()]
+            with open(path, "w") as file:
+                if not code in codes:
+                    codes.append(code)
+                file.write(u"\n".join(codes))
+
+        def remove(path, code):
+            codes = [line.strip() for line in open(path).readlines()]
+            if codes[0] == code:
+                print("That is the default, change default first", file=stderr)
+            elif code in codes:
+                codes.remove(code)
+                with open(path, "w") as file:
+                    file.write(u"\n".join(codes))
+
+        def default(path, code):
+            codes = [line.strip() for line in open(path).readlines()]
+            if codes[0] == code:
+                return
+            else:
+                if code in codes: codes.remove(code)
+                codes.insert(0, code)
+                with open(path, "w") as file:
+                    file.write(u"\n".join(codes))
+
+        def translate(language, Text, Note, db):
+            translations = {}
+            try:
+                translations =  loads(open(f"languages/{language}.json").read())
+            except FileNotFoundError:
+                pass
+            for key, value in translations.items():
+                try:
+                    text = Text.query.get(key) or Text(key)
+                    note = Note.query.get({
+                        "text": key,
+                        "code": language,
+                    }) or Note(value, language, key)
+                    note.verified = True
+                    db.session.add(text)
+                    db.session.add(note)
+                    db.session.commit()
+                except:
+                    db.session.rollback()
+                    raise
+
+        self.update = update
+        self.upgrade = upgrade
+        self.list = list
+        self.install = install
+        self.add = add
+        self.remove = remove
+        self.default = default
+        self.translate = translate
+
+    def init_app(self, app, db):
+        from app.models import Code, Text, Note
+        @app.cli.group()
+        @pass_context
+        def languages(context):
+            """Translation and localization commands"""
+            pass
+
         @languages.command()
-        @argument("input", type=File("r", "UTF8"))
-        @argument("language")
-        def install(input, language="zh"):
+        @argument("output", default="languages/index.json")
+        def update(output):
+            """
+            Update list of available languages
+            Saved to OUTPUT
+            Example:
+            >>>flask languages update languages.json
+            """
+            self.update(output, app.config["TRANSLATION_KEY"])
+
+        @languages.command()
+        def list(Code):
+            """
+            List installed languages
+            These locales are in the database and will offer a menu option to clients
+            These are the languages with "allowed" set to true
+            """
+            for code in self.list(Code):
+                print(code)
+
+        @languages.command()
+        def upgrade():
+            """
+            Upload all available languages to database
+            This should be run after an update, or to initialise languages
+            These languages won't be set to "allowed"
+            """
+            self.upgrade(db, Text, Code)
+            self.install(db, Code)
+            for code in self.list(Code):
+                self.translate(code.id, Text, Note, db)
+        
+        @languages.command()
+        def install():
             """
             Install a new language from INPUT
             Saves the language into the database
             """
-            print()
-            try:
-                languages = loads(input.read())
-                name = languages[language]
-                db.session.add(Text(name))
-                db.session.flush()
-                db.session.add((Code(language, name)))
-                db.session.commit()
-            except IntegrityError as e:
-                db.session.rollback()
-                print(f"{name}, {language} is already installed", file=stderr)
-            except: raise
-            print("Currently Installed:")
-            for code in Code.query.all():
-                print(f"{code.id}:{code.text}")
+            self.install(db, Code)
+
+        @languages.command()
+        @argument("language")
+        def add(language):
+            """
+            Add a language to allowed languages
+            Update database with installed languages
+            """
+            self.add("languages/allowed", language)
+            self.install(db, Code)
+
+        @languages.command()
+        @argument("language")
+        def remove(language):
+            """
+            Remove a language from allowed list
+            Can't remove default language
+            Must set new default first
+            Update database with installed languages
+            """
+            self.remove("languages/allowed", language)
+            self.install(db, Code)
+
+        @languages.command()
+        @argument("language")
+        def default(language):
+            """
+            Set the default language
+            Will be added if not currently allowed
+            Update database with installed languages
+            """
+            self.default("languages/allowed", language)
+            self.install(db, Code)
